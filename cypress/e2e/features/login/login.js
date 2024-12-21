14bb2c2b05ab1de2861dd78b0ac123ea4cea694a
VirajDilshanKumarage <viraj123e@gmail.com>
2024-12-21 14:14:00 +0530

    Add - dev - add login page objects model

--- cypress/e2e/features/login/login.js
+++ cypress/e2e/features/login/login.js
@@ -1,23 +1,20 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import LoginPage from "./loginPage";
+
+
 
 Given("I am on the login page", () => {
-  cy.visit("http://zero.webappsecurity.com/login.html"); // Visit Sauce Demo login page
+  LoginPage.visit();
 });
 
 When("I enter valid credentials", () => {
-  cy.get('#user_login') // Username input field
-    .type('username'); // Use a valid username provided by Sauce Demo
+  LoginPage.enterUserName('username');
+  LoginPage.enterPassword('password');
+  LoginPage.submit();
 
-  cy.get('#user_password') // Password input field
-    .type('password'); // Use the provided password
-
-  cy.get('input[name="submit"]') // Login button
-    .should("exist")
-    .click(); // Click the login button
 });
 
 Then("I should see the home page", () => {
-
-  cy.get("#account_summary_tab").should("be.visible"); // Validate that the products are visible
+  LoginPage.seeHomePage();
 }); 
     
