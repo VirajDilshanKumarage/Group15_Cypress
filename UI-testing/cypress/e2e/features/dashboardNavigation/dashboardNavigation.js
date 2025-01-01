5db6ada4e2725cc1a67e94846f9c5bba08172dc6
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2025-01-01 11:38:05 +0530

    update UI test cases

--- UI-testing/cypress/e2e/features/dashboardNavigation/dashboardNavigation.js
+++ UI-testing/cypress/e2e/features/dashboardNavigation/dashboardNavigation.js
@@ -11,8 +11,8 @@ Given("I am logged in as a valid user", () => {
 When("I navigate to each tab in the dashboard", () => {
   DashboardPage.tabs.forEach((tab) => {
     DashboardPage.navigateToTab(tab.id);
-    // cy.get('.oxd-layout-container').should('be.visible');
-    cy.wait(1000); // Allow the page to load
+    cy.get('.oxd-main-menu-item-wrapper').should('be.visible');
+    // cy.wait(1000); // Allow the page to load
   });
 });
 
