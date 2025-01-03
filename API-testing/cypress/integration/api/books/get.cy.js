90a77db867a5549db111fbcfc10d286f7cbf8c84
VirajDilshanKumarage <viraj123e@gmail.com>
2025-01-03 16:10:15 +0530

    Modification -jenkin-check -comment all other get test case except get all books as admin to check on jenkings

--- API-testing/cypress/integration/api/books/get.cy.js
+++ API-testing/cypress/integration/api/books/get.cy.js
@@ -16,78 +16,78 @@ describe('API Testing: GET APIs', () => {
     });
 
   
-   //Test get book by ID - admin 
-    it('Should fetch a specific book by ID successfully', () => {
-        const bookId = 3; 
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/${bookId}`,
-            auth: adminCredentials,
-        }).then((response) => {
-            expect(response.status).to.eq(200); 
-            expect(response.body).to.have.property('id', bookId); 
-            expect(response.body).to.have.property('title'); 
-            expect(response.body).to.have.property('author'); 
-        });
-    });
+//    //Test get book by ID - admin 
+//     it('Should fetch a specific book by ID successfully', () => {
+//         const bookId = 3; 
+//         cy.request({
+//             method: 'GET',
+//             url: `${baseUrl}/${bookId}`,
+//             auth: adminCredentials,
+//         }).then((response) => {
+//             expect(response.status).to.eq(200); 
+//             expect(response.body).to.have.property('id', bookId); 
+//             expect(response.body).to.have.property('title'); 
+//             expect(response.body).to.have.property('author'); 
+//         });
+//     });
 
-    //Test for fetching a non-existing book - admin
-    it('Should return 404 for a non-existing book ID', () => {
-        const invalidBookId = 999; 
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/${invalidBookId}`,
-            auth: adminCredentials,
-            failOnStatusCode: false // Prevent Cypress from failing the test on non-2xx responses
-        }).then((response) => {
-            expect(response.status).to.eq(404); 
-            cy.log('Response Body:', response.body); 
-            expect(response.body).to.eq('Book not found');
-        });
-    });
+//     //Test for fetching a non-existing book - admin
+//     it('Should return 404 for a non-existing book ID', () => {
+//         const invalidBookId = 999; 
+//         cy.request({
+//             method: 'GET',
+//             url: `${baseUrl}/${invalidBookId}`,
+//             auth: adminCredentials,
+//             failOnStatusCode: false // Prevent Cypress from failing the test on non-2xx responses
+//         }).then((response) => {
+//             expect(response.status).to.eq(404); 
+//             cy.log('Response Body:', response.body); 
+//             expect(response.body).to.eq('Book not found');
+//         });
+//     });
     
 
 
 
-    //Test get all books for user role
-    it('Should fetch all books successfully', () => {
-        cy.request({
-            method: 'GET',
-            url: baseUrl,
-            auth: userCredentials,
-        }).then((response) => {
-            expect(response.status).to.eq(200); 
-            expect(response.body).to.be.an('array'); 
-        });
-    });
+//     //Test get all books for user role
+//     it('Should fetch all books successfully', () => {
+//         cy.request({
+//             method: 'GET',
+//             url: baseUrl,
+//             auth: userCredentials,
+//         }).then((response) => {
+//             expect(response.status).to.eq(200); 
+//             expect(response.body).to.be.an('array'); 
+//         });
+//     });
 
-    //Test to fetch a book by ID
-    it('Should fetch a specific book by ID successfully', () => {
-        const bookId = 3; 
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/${bookId}`,
-            auth: userCredentials,
-        }).then((response) => {
-            expect(response.status).to.eq(200); 
-            expect(response.body).to.have.property('id', bookId); 
-            expect(response.body).to.have.property('title'); 
-            expect(response.body).to.have.property('author'); 
-        });
-    });
+//     //Test to fetch a book by ID
+//     it('Should fetch a specific book by ID successfully', () => {
+//         const bookId = 3; 
+//         cy.request({
+//             method: 'GET',
+//             url: `${baseUrl}/${bookId}`,
+//             auth: userCredentials,
+//         }).then((response) => {
+//             expect(response.status).to.eq(200); 
+//             expect(response.body).to.have.property('id', bookId); 
+//             expect(response.body).to.have.property('title'); 
+//             expect(response.body).to.have.property('author'); 
+//         });
+//     });
 
-    // Test for fetching a non-existing book
-    it('Should return 404 for a non-existing book ID', () => {
-        const invalidBookId = 999; 
-        cy.request({
-            method: 'GET',
-            url: `${baseUrl}/${invalidBookId}`,
-            auth: userCredentials,
-            failOnStatusCode: false 
-        }).then((response) => {
-            expect(response.status).to.eq(404); 
-            cy.log('Response Body:', response.body); 
-            expect(response.body).to.eq('Book not found');
-        });
-    });
+//     // Test for fetching a non-existing book
+//     it('Should return 404 for a non-existing book ID', () => {
+//         const invalidBookId = 999; 
+//         cy.request({
+//             method: 'GET',
+//             url: `${baseUrl}/${invalidBookId}`,
+//             auth: userCredentials,
+//             failOnStatusCode: false 
+//         }).then((response) => {
+//             expect(response.status).to.eq(404); 
+//             cy.log('Response Body:', response.body); 
+//             expect(response.body).to.eq('Book not found');
+//         });
+//     });
 });
